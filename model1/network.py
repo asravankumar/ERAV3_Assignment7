00219047200c77f7d04d02154ff83ed1ff2ba2f9
A Sravan Kumar <sravan@caavo.com>
2024-12-22 15:30:46 +0530

    first cut model

---
+++ model1/network.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input image size : 28x28
+
+        # Convolution Layer 1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=3, padding=1, bias=False), # Input Channels - 1, Output Channels - 12
+            nn.ReLU()
+        )
+        
+        # Convolution Layer 2
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(10, 16, kernel_size=3, padding=1, bias=False),
+            nn.ReLU()
+        )
+        
+        # Transition Layer 1 - MaxPool and 1x1 conv
+        self.transition1 = nn.Sequential(
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 10, kernel_size=1, padding=0, bias=False),
+        )
+
+        # Convolution Layer 3
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(10, 16, kernel_size=3, padding=1, bias=False),
+            nn.ReLU()
+        )
+
+        # Convolution Layer 4
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3, padding=1, bias=False),
+            nn.ReLU()
+        )
+
+        # Transition Layer 2 - MaxPool and 1x1 conv
+        self.transition2 = nn.Sequential(
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 10, kernel_size=1, padding=0, bias=False),
+        )
+        
+        # Convolution Layer 5
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(10, 16, kernel_size=3, padding=1, bias=False),
+            nn.ReLU()
+        )
+
+        # Convolution Layer 6
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(16, 10, kernel_size=3, padding=0, bias=False),
+            nn.ReLU()
+        )
+        
+        self.fc = nn.Linear(10 * 5 * 5, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+
+        x = self.transition1(x)
+
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.transition2(x)
+
+        x = self.conv5(x)
+        x = self.conv6(x)
+  
+        x = x.view(x.size(0), -1)
+
+        x = self.fc(x)
+        return F.log_softmax(x, dim=-1)
